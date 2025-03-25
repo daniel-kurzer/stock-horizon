112e3844d378730e1083121bd7583e1071a5f090
Daniel Kurzer <121629742+daniel-kurzer@users.noreply.github.com>
2025-03-25 14:01:19 +0100

    Sidebar & Navbar Toggle

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,12 +1,25 @@
 "use client";
 import React from "react";
 import { Menu } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/app/state";
 
 const index = () => {
+
+  const dispatch = useDispatch();
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed)
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+  }
+
+  const sidebarClassNames = `fixed flex flex-col ${isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"} bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
+
   return (
-    <div>
+    <div className={sidebarClassNames}>
       {/* TOP LOGO */}
-      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8 m-auto">
         <img
           className="w-36 h-36"
           src="STOCK_HORIZON_Logo.png"
@@ -14,8 +27,8 @@ const index = () => {
         />
 
         <button
-          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-purple-300"
-          onClick={() => {}}
+          className="px-3 py-3 bg-gray-100 rounded-full hover:bg-purple-300"
+          onClick={toggleSidebar}
         >
           <Menu className="h-4 w-4" />
         </button>
