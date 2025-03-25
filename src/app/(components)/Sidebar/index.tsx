6d2fb5568e4db60278e5a00f5cf70fe143157fd5
Daniel Kurzer <121629742+daniel-kurzer@users.noreply.github.com>
2025-03-25 16:11:13 +0100

    SidebarLink-Setup

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,9 +1,31 @@
 "use client";
 import React from "react";
-import { Menu } from "lucide-react";
+import { Archive, Clipboard, Layout, LucideIcon, Menu, SlidersHorizontal, User } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { useAppSelector } from "@/app/redux";
 import { setIsSidebarCollapsed } from "@/app/state";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+interface SidebarLinkProps {
+  href: string; icon: LucideIcon; label: string; isCollapsed: boolean
+}
+
+const SidebarLink = ({
+  href, icon: Icon, label, isCollapsed
+} : SidebarLinkProps) => {
+  const pathname = usePathname()
+  const isActive = pathname === href || (pathname === "/" && href === "/dashboard")
+
+  return(
+    <Link href={href}>
+      <div className={`cursor-pointer flex items-center ${isCollapsed ? "justify-center py-4" : "justify-start py-4 px-8"} hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${isActive ? "bg-blue-200 text-white" : ""}`}>
+        <Icon className="h-6 w-6 !text-gray-700" />
+        <span className={`${isCollapsed ? "hidden" : "block"} font-medium text-gray-700`}>{label}</span>
+      </div>
+    </Link>
+  )
+}
 
 const index = () => {
 
@@ -19,24 +41,24 @@ const index = () => {
   return (
     <div className={sidebarClassNames}>
       {/* TOP LOGO */}
-      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8 m-auto">
+      <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 m-auto ${isSidebarCollapsed ? "px-1" : "px-8"}`}>
         <img
-          className="w-36 h-36"
-          src="STOCK_HORIZON_Logo.png"
+          className={`w-20 h-20 transition-all duration-500 ${isSidebarCollapsed ? "w-12 h-12" : "w-20 h-20"}`}
+          src="SH-Logo.png"
           alt="Logo"
         />
+        <h1 className={`font-extrabold text-lg drop-shadow-xl text-gray-700 ${isSidebarCollapsed ? "hidden" : "block"}`}>STOCK HORIZON</h1>
 
-        <button
-          className="px-3 py-3 bg-gray-100 rounded-full hover:bg-purple-300"
-          onClick={toggleSidebar}
-        >
-          <Menu className="h-4 w-4" />
-        </button>
+        
       </div>
       
       {/* Links */}
     <div className="flex-grow mt-8">
-      {/* Insert Links */}
+      <SidebarLink href="/dashboard" icon={Layout} label="Dashboard" isCollapsed={isSidebarCollapsed} />
+      <SidebarLink href="/inventory" icon={Archive} label="Inventory" isCollapsed={isSidebarCollapsed} />
+      <SidebarLink href="/products" icon={Clipboard} label="Products" isCollapsed={isSidebarCollapsed} />
+      <SidebarLink href="/users" icon={User} label="Users" isCollapsed={isSidebarCollapsed} />
+      <SidebarLink href="/settings" icon={SlidersHorizontal} label="Settings" isCollapsed={isSidebarCollapsed} />
     </div>
 
     {/* FOOTER */}
